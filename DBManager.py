38698fd619b234e45b0ff79a5b98bee243026656
Skylar024 <144626015+Skylar024@users.noreply.github.com>
2024-05-12 12:20:16 -0400

    Add files via upload

--- DBManager.py
+++ DBManager.py
@@ -4,8 +4,7 @@ Author: Skylar Thompson
 This program is the Database control module
 """
 import sqlite3
-#import csv
-import appointment as A
+
 
 conn = sqlite3.connect('iShop.db')
 curs = conn.cursor()
@@ -18,12 +17,7 @@ def submit(business_name, customer_name, set_date):
     Database, no return needed (unless wanted for a conformation message to
     the user for a successful submission)"""
     business_id = get_business_id(business_name)
-    
-    
-    # sql = f"INSERT INTO scheduleDB (business_id, business_name, customer_name, set_date) VALUES ({business_id}, '{business_name}', '{customer_name}', '{set_date}')"
-    # curs.execute(sql)
-    # conn.commit()
-    
+
     curs.execute("INSERT INTO scheduleDB VALUES (:business_id, :business_name, :customer_name, :set_date)",
         {
             'business_id': business_id,
@@ -34,13 +28,15 @@ def submit(business_name, customer_name, set_date):
     conn.commit()
 
 def get_business_id(BN):
+    """This Controls getting the business_id for the business_name that is provided from the business_service_table.
+    Intended to be used as a nested function for the submit() function."""
     sql = f"SELECT DISTINCT business_id FROM business_service_table WHERE business_name = '{BN}'"
     curs.execute(sql)
     temp = curs.fetchone()
     temp2 = temp[0]
-    return(temp2)
-    
-def delete_appt():
+    return temp2
+
+def delete_appt():          #Currently unsed code, may possibly implement later
     """This controls deleting entries from the Database
     Intended to be used by getting the business_id from the users input from
     the GUI using the business_id.get() from a 'delete appointment' button (or
@@ -51,7 +47,7 @@ def delete_appt():
     curs.execute(sql)
     conn.commit()
 
-def check_appt():
+def check_appt():           #Currently unsed code, may possibly implement later
     """This controls checking entries in the Database
     Intended to be used by getting the date from the users input from the GUI
     using the set_date.get() from a 'check date available' button (or something like it).
@@ -65,7 +61,7 @@ def check_appt():
         record_list += "Business ID: " + str(x[0]) + "\t\t" + "Business Name: " + str(x[1]) + "\t\t" + "Customer Name: " + str(x[2]) + "\t\t" + "Appointment Date: " + str(x[3]) + "\n"
     return record_list
 
-def print_database():
+def print_database():       #Currently unsed code, may possibly implement later. But IS USED for testing
     """This controls printing the entire Database
     Intended to be used by returning the entire Database when a 'print all
     appointments' button (or something like it) is clicked in the GUI"""
@@ -75,8 +71,8 @@ def print_database():
     for x in temp:
         # records_list += x
         print(x)
-        
-def print_serviceDB():
+
+def print_serviceDB():      #Currently unsed code, may possibly implement later. But IS USED for testing
     """This controlls printing the entire serviceDB"""
     curs.execute("SELECT * FROM serviceDB")
     temp = curs.fetchall()
@@ -84,13 +80,16 @@ def print_serviceDB():
         print(x)
 
 def check_business_from_service(service):
-    
+    """This Controls providing the GUI with all business(s) that provide the selected service that was input from the GUI
+    Intended to be used to return business_id and business_name that have the service that was selected in the GUI"""
     sql = f"SELECT business_id, business_name FROM business_service_table WHERE service = '{service}'"
     curs.execute(sql)
     temp = curs.fetchall()
     return temp
 
 def return_services():
+    """This Controls providing the GUI with every single unique service and service_id that all the business provide
+    Intended to be used to display all services to the User before a selection for a service is chosen"""
     curs.execute("SELECT DISTINCT service_id, service FROM business_service_table")
     temp = curs.fetchall()
     return temp
